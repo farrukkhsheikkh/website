b6a2b139416d313a7f46a62c86ad05402fbd18b3
farrukkhsheikkh <farrukkh.sheikkh@gmail.com>
2022-09-25 04:33:24 +0500

    Add files via upload

---
+++ products/game/src/game/config.js
@@ -0,0 +1,22 @@
+pandaConfig = {
+    name: 'Super Saeen',
+    version: '1.0.1',
+    
+    system: {
+        width: 768,
+        height: 1024,
+        rotateScreen: false,
+        bgColor: '#000000'
+    },
+    analytics: {
+        id: 'UA-69527731-1'
+    },
+    storage: {
+        id: 'farrukkh.com/game'
+    },
+    iOS8: {
+        system: {
+            webGL: true
+        }
+    }
+};
